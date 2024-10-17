4d158e6572714c324fc9afc81798e199bd028dc9
Manosmita Sahu <manosmita.sahu.05@gmail.com>
2024-10-18 01:24:37 +0530

    stopwatch

---
+++ script.js
@@ -0,0 +1,51 @@
+document.addEventListener("DOMContentLoaded", function() {
+let display =  document.getElementById("main");
+let timer = null;
+let startTime = 0;
+let elapsedTime = 0;
+let isRunning = false;
+
+
+
+function startTimer(){
+    if(!isRunning){
+        startTime = Date.now()-elapsedTime;
+        timer = setInterval(updatedisplay, 10);
+        isRunning = true;
+    }
+    
+    
+}
+
+function stopTimer(){
+    clearInterval(timer);
+    isRunning = false;
+
+}
+
+function resetTimer(){
+    isRunning = false;
+    clearInterval(timer);
+    elapsedTime = 0;
+    display.textContent =` 00:00:00:00`;
+
+}
+
+function updatedisplay(){
+    const currentTime = Date.now();
+    elapsedTime = currentTime-startTime;
+    let hours = Math.floor(elapsedTime / (1000 * 60 * 60)).toString().padStart(2, '0');
+    let minutes = Math.floor((elapsedTime / (1000 * 60)) % 60).toString().padStart(2, '0');
+    let seconds = Math.floor((elapsedTime / 1000) % 60).toString().padStart(2, '0');
+    let milliseconds = Math.floor((elapsedTime % 1000) / 10).toString().padStart(2, '0');
+    display.textContent = `${hours}:${minutes}:${seconds}:${milliseconds}`;
+
+
+}
+
+document.getElementById("startbtn").addEventListener("click",startTimer);
+document.getElementById("stopbtn").addEventListener("click",stopTimer);
+document.getElementById("resetbtn").addEventListener("click",resetTimer);
+ 
+
+});
